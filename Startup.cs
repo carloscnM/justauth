c0c83f02c15a7b3c5ff0ec752020085cc061168c
Carlos Mario <oliveiracarlosmario1@gmail.com>
2020-07-28 17:53:43 -0300

    Segregating servce directories

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,7 @@
 using System.Text;
 using justauth.src.Infrastructure.DataBase;
 using justauth.src.Models;
-using justauth.src.Services;
+using justauth.src.Services.JWT;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
